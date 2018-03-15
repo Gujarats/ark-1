5a2232c750df89684986289ae1177b2e47765299
Gujarat Santana <gujarat.santana@traveloka.com>
2018-03-15 14:15:34 +0700

    new feature rotate, and store

--- iam.go
+++ iam.go
@@ -1,36 +1,149 @@
-package main
+package ark
 
 import (
-	"log"
-
 	"github.com/aws/aws-sdk-go/aws"
 	"github.com/aws/aws-sdk-go/aws/session"
 	"github.com/aws/aws-sdk-go/service/iam"
+	"github.com/aws/aws-sdk-go/service/kms"
 )
 
-func CreateAccessKey() (string, error) {
+const (
+	MaxIAMUser = 2
+	Active     = "Active"
+	Inactive   = "Inactive"
+)
 
+// create session specific region
+func CreateSession(region string) (*session.Session, error) {
 	sess, err := session.NewSession(
 		&aws.Config{
-			Region: aws.String(env.Region),
+			Region: aws.String(region),
 		},
 	)
 
 	if err != nil {
-		log.Fatal(err)
+		return nil, err
 	}
 
-	// Create a IAM service client.
+	return sess, nil
+}
+
+// Create new access/secret keys
+// following the best practice AWS
+func CreateAccessKey(sess *session.Session, userName string) (*iam.AccessKey, error) {
 	svc := iam.New(sess)
 
+	listAccessKeys, err := svc.ListAccessKeys(&iam.ListAccessKeysInput{
+		UserName: aws.String(userName),
+	})
+	if err != nil {
+		return nil, err
+	}
+
+	err = deleteInactiveKey(svc, userName, listAccessKeys)
+	if err != nil {
+		return nil, err
+	}
+
 	result, err := svc.CreateAccessKey(&iam.CreateAccessKeyInput{
-		UserName: aws.String(env.User),
+		UserName: aws.String(userName),
+	})
+	if err != nil {
+		return nil, err
+	}
+
+	return result.AccessKey, nil
+}
+
+// if All active then delete the first key
+// if found inactive then delete it
+// for avoid creating new keys failure if IAM user is equal MAxIAMUser
+func deleteInactiveKey(svc *iam.IAM, userName string, listAccessKeys *iam.ListAccessKeysOutput) error {
+	// delete the deactivate key if keys if == 2
+	var foundInactive bool
+	if len(listAccessKeys.AccessKeyMetadata) == MaxIAMUser {
+		for _, accessKey := range listAccessKeys.AccessKeyMetadata {
+
+			if *accessKey.Status == Inactive {
+				foundInactive = true
+				_, err := svc.DeleteAccessKey(&iam.DeleteAccessKeyInput{
+					AccessKeyId: accessKey.AccessKeyId,
+					UserName:    aws.String(userName),
+				})
+
+				if err != nil {
+					return err
+				}
+			}
+
+			if *accessKey.Status == Active {
+				deactivateKey(svc, userName, *accessKey.AccessKeyId)
+			}
+		}
+
+		if !foundInactive {
+			_, err := svc.DeleteAccessKey(&iam.DeleteAccessKeyInput{
+				AccessKeyId: listAccessKeys.AccessKeyMetadata[0].AccessKeyId,
+				UserName:    aws.String(userName),
+			})
+
+			deactivateKey(svc, userName, *listAccessKeys.AccessKeyMetadata[1].AccessKeyId)
+
+			if err != nil {
+				return err
+			}
+		}
+	}
+
+	return nil
+}
+
+// before creating new one deactivate last key
+func deactivateKey(svc *iam.IAM, userName string, accessKeyId string) error {
+	_, err := svc.UpdateAccessKey(&iam.UpdateAccessKeyInput{
+		UserName:    aws.String(userName),
+		Status:      aws.String(Inactive),
+		AccessKeyId: aws.String(accessKeyId),
 	})
 
 	if err != nil {
-		log.Fatal(err)
+		return err
 	}
 
-	return result.AccessKey.String(), nil
+	return nil
+}
 
+// Store the keys to parameter store
+// the key is /bei/developers/s3read/access for access key
+// the key is /bei/developers/s3read/secret for secret key
+func StoreKeys(sess *session.Session, accessKey *iam.AccessKey) error {
+	svc := kms.New(sess)
+
+	err := createParameterStoreKey(svc, "/bei/developers/s3read/access", *accessKey.AccessKeyId)
+	err = createParameterStoreKey(svc, "/bei/developers/s3read/secret", *accessKey.SecretAccessKey)
+
+	if err != nil {
+		return err
+	}
+
+	return nil
+
+}
+
+func createParameterStoreKey(svc *kms.KMS, key, value string) error {
+	// Create the key
+	_, err := svc.CreateKey(&kms.CreateKeyInput{
+		Tags: []*kms.Tag{
+			{
+				TagKey:   aws.String(key),
+				TagValue: aws.String(value),
+			},
+		},
+	})
+
+	if err != nil {
+		return err
+	}
+
+	return nil
 }
